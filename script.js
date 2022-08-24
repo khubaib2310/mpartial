76fc6b84376cb15accb19808b6ca414267dc7b21
khubaib2310 <khubaibmuhammad964@gmail.com>
2022-08-24 16:17:29 +0500

    mpartial final version

--- script.js
+++ script.js
@@ -22,3 +22,15 @@ function defaultDisplay() {
   }
 }
 window.addEventListener("resize", defaultDisplay);
+
+const slideValue = document.querySelector("span");
+const inputSlider = document.querySelector("input");
+inputSlider.oninput = () => {
+  let value = inputSlider.value;
+  slideValue.textContent = value;
+  slideValue.style.left = value / 2 + "%";
+  slideValue.classList.add("show");
+};
+inputSlider.onblur = () => {
+  slideValue.classList.remove("show");
+};
