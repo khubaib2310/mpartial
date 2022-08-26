b0c58d2ee17da55e1a047bdd244bdb788d0c9e8e
khubaib2310 <111236502+khubaib2310@users.noreply.github.com>
2022-08-26 15:21:55 +0500

    new

--- script.js
+++ script.js
@@ -23,6 +23,8 @@ function defaultDisplay() {
 }
 window.addEventListener("resize", defaultDisplay);
 
+// ---------- slider --------
+
 const slideValue = document.querySelector("span");
 const inputSlider = document.querySelector("input");
 inputSlider.oninput = () => {
@@ -33,4 +35,76 @@ inputSlider.oninput = () => {
 };
 inputSlider.onblur = () => {
   slideValue.classList.remove("show");
+};
+
+const range = document.getElementById("range");
+
+range.addEventListener("input", (e) => {
+  const value = +e.target.value;
+  const label = e.target.nextElementSibling;
+
+  const range_width = getComputedStyle(e.target).getPropertyValue("width");
+  const label_width = getComputedStyle(label).getPropertyValue("width");
+
+  const num_width = +range_width.substring(0, range_width.length - 2);
+  const num_label_width = +label_width.substring(0, label_width.length - 2);
+
+  const max = +e.target.max;
+  const min = +e.target.min;
+
+  const left =
+    value * (num_width / max) -
+    num_label_width / 2 +
+    scale(value, min, max, 10, -10);
+
+  label.style.left = `${left}px`;
+
+  label.innerHTML = value;
+});
+
+// https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
+const scale = (num, in_min, in_max, out_min, out_max) => {
+  return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
+};
+
+const slideValues = document.querySelector("span");
+const inputSliders = document.querySelector("input");
+inputSlider.oninput = () => {
+  let value = inputSlider.value;
+  slideValue.textContent = value;
+  slideValue.style.left = value / 2 + "%";
+  slideValue.classList.add("show");
+};
+inputSlider.onblur = () => {
+  slideValue.classList.remove("show");
+};
+
+const ranges = document.getElementById("ranges");
+
+ranges.addEventListener("input", (e) => {
+  const value = +e.target.value;
+  const label = e.target.nextElementSibling;
+
+  const range_width = getComputedStyle(e.target).getPropertyValue("width");
+  const label_width = getComputedStyle(label).getPropertyValue("width");
+
+  const num_width = +range_width.substring(0, range_width.length - 2);
+  const num_label_width = +label_width.substring(0, label_width.length - 2);
+
+  const max = +e.target.max;
+  const min = +e.target.min;
+
+  const left =
+    value * (num_width / max) -
+    num_label_width / 2 +
+    scale(value, min, max, 10, -10);
+
+  label.style.left = `${left}px`;
+
+  label.innerHTML = value;
+});
+
+// https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
+const scales = (num, in_min, in_max, out_min, out_max) => {
+  return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
 };
